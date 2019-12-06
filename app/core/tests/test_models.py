101fe958cb1b6ba4c8eed6e3dcba913adec45837
Adrian Fernandez Vigo <Adrian.FernandezVigo@dyson.com>
2019-12-06 13:09:34 +0000

    Add custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized"""
+        email = 'mail@MAIL.com'
+        user = get_user_model().objects.create_user(email, 'Testpass')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'Testpass')
+
+    def test_create_new_superuser(self):
+        """ Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@test.com',
+                                                         'Testpass')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
