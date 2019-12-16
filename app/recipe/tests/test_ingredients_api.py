dc8b519b7ebe9097d0a77d0aca508b3d0b3401a3
Adrian Fernandez Vigo <Adrian.FernandezVigo@dyson.com>
2019-12-16 11:23:37 +0000

    Ingredient creation

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -48,7 +48,8 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
     def test_ingredients_limited_to_user(self):
-        """ Test that only ingredients for the authenticated user are returned """
+        """ Test that only ingredients for the authenticated user
+        are returned """
         user2 = get_user_model().objects.create_user(
             'other@test.com',
             'Testpass'
@@ -61,3 +62,19 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """ Test create a new ingredient """
+        payload = {'name': "Cabbage"}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(user=self.user,
+                                           name=payload['name']).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """ Test create an ingredient with an invalid payload fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENT_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
